1d6ef275ea7c9ea53752f5f8ac69d8b4759a6f61
Samiul Saki <samiulsaki@gmail.com>
2017-09-20 18:36:00 +0200

    Search function updated @ Wed Sep 20 18:36:00 CEST 2017

--- com.vehicle.rest/src/com/vehicle/dao/SchemaVehicle.java
+++ com.vehicle.rest/src/com/vehicle/dao/SchemaVehicle.java
@@ -24,6 +24,85 @@ import com.vehicle.util.ToJSON;
 public class SchemaVehicle extends OracleVehicle {
 
 	
+	// VEHICLE_REGS_REG, VEHICLE_REGS_BRAND, VEHICLE_REGS_MODEL, VEHICLE_REGS_MANU_YEAR, VEHICLE_REGS_OWNER_FIRST_NAME, VEHICLE_REGS_OWNER_LAST_NAME, VEHICLE_REGS_FIRST_REG " +
+	/**
+	 * This method will insert a record into the VEHICLE_REGS table. 
+	 * 
+	 * Note: there is no validation being done... if this was a real project you
+	 * must do validation here!
+	 * 
+	 * @param VEHICLE_REGS_REG
+	 * @param VEHICLE_REGS_BRAND
+	 * @param VEHICLE_REGS_MODEL
+	 * @param VEHICLE_REGS_MANU_YEAR - integer column
+	 * @param VEHICLE_REGS_OWNER_FIRST_NAME
+	 * @param VEHICLE_REGS_OWNER_LAST_NAME
+	 * @param VEHICLE_REGS_FIRST_REG - date column
+	 * @return integer 200 for success, 500 for error
+	 * @throws Exception
+	 */
+	public int insertIntoVEHICLE_REGS(String VEHICLE_REGS_REG, 
+											String VEHICLE_REGS_BRAND, 
+											String VEHICLE_REGS_MODEL, 
+											String VEHICLE_REGS_MANU_YEAR, 
+											String VEHICLE_REGS_OWNER_FIRST_NAME,
+											String VEHICLE_REGS_OWNER_LAST_NAME,
+											String VEHICLE_REGS_FIRST_REG) 
+										throws Exception {
+
+		PreparedStatement query = null;
+		Connection conn = null;
+
+		try {
+			/*
+			 * If this was a real application, you should do data validation here
+			 * before starting to insert data into the database.
+			 * 
+			 * Important: The primary key on VEHICLE_REGS table will auto increment.
+			 * 		That means the VEHICLE_REGS_REG column does not need to be apart of the 
+			 * 		SQL insert query below.
+			 */
+			conn = oracleVehicleRegsConnection();
+			query = conn.prepareStatement("insert into VEHCILE_REGS " +
+					"(VEHICLE_REGS_REG, VEHICLE_REGS_BRAND, VEHICLE_REGS_MODEL, VEHICLE_REGS_MANU_YEAR, VEHICLE_REGS_OWNER_FIRST_NAME, VEHICLE_REGS_OWNER_LAST_NAME, VEHICLE_REGS_FIRST_REG) " +
+					"VALUES ( ?, ?, ?, ?, ? , ? , ?) ");
+
+			query.setString(1, VEHICLE_REGS_REG);
+			query.setString(2, VEHICLE_REGS_BRAND);
+			query.setString(3, VEHICLE_REGS_MODEL);
+
+			//VEHICLE_REGS_MANU_YEAR is a number column, so we need to convert the String into a integer
+			int avilInt = Integer.parseInt(VEHICLE_REGS_MANU_YEAR);
+			query.setInt(4, avilInt);
+
+			query.setString(5, VEHICLE_REGS_OWNER_FIRST_NAME);
+			query.setString(6,  VEHICLE_REGS_OWNER_LAST_NAME);
+			
+			//String[] splitArray = VEHICLE_REGS_FIRST_REG.split("-");
+			//int day = Integer.parseInt(splitArray[0]);
+			//int month = Integer.parseInt(splitArray[1]);
+			//int year = Integer.parseInt(splitArray[2]);			
+
+			query.setString(7,  VEHICLE_REGS_FIRST_REG);
+			query.executeUpdate(); //note the new command for insert statement
+
+		} catch(Exception e) {
+			e.printStackTrace();
+			return 500; //if an error occurs, return a 500
+		}
+		finally {
+			if (conn != null) conn.close();
+		}
+
+		return 200;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	/**
--- com.vehicle.rest/src/com/vehicle/rest/inventory/V2_inventory.java
+++ com.vehicle.rest/src/com/vehicle/rest/inventory/V2_inventory.java
@@ -1,13 +1,17 @@
 package com.vehicle.rest.inventory;
 
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jettison.json.JSONArray;
 
 import com.vehicle.dao.SchemaVehicle;
@@ -38,6 +42,11 @@ public class V2_inventory {
 		
 		try {
 			
+			if(reg == null) {
+				return Response.status(400).entity("Error: please specify registration for this search").build();
+				
+			}
+			
 			SchemaVehicle dao = new SchemaVehicle();
 			
 			json = dao.queryReturnVehicleRegs(reg);
@@ -51,4 +60,171 @@ public class V2_inventory {
 		
 		return Response.ok(returnString).build();
 	}
+	
+	/*
+	 * This method can be used if the method returnBrandParts is not used.
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnErrorOnBrand() throws Exception {
+		
+		return Response.status(400).entity("Error: please specify brand for this search").build();
+	}
+	*/
+	
+	/**
+	 * This method will return the specific brand of PC parts the user is looking for.
+	 * It is very similar to the method returnBrandParts except this method uses the 
+	 * PathParam to bring in the data.
+	 * 
+	 * Example would be:
+	 * http://localhost:7001/com.vehicle.rest/api/v2/inventory/<registration>
+	 * 
+	 * @param reg - vehicle registration number
+	 * @return - json array results list from the database
+	 * @throws Exception
+	 */
+	@Path("/{reg}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response returnRegs(
+				@PathParam("reg") String reg) 
+				throws Exception {
+		
+		String returnString = null;
+		
+		JSONArray json = new JSONArray();
+		
+		try {
+			
+			SchemaVehicle dao = new SchemaVehicle();
+			
+			json = dao.queryReturnVehicleRegs(reg);
+			returnString = json.toString();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+	
+
+	/**
+	 * This method does a search on both product and the product item number.
+	 * It uses PathParam to bring in both parameters.
+	 * 
+	 * Example:
+	 * http://localhost:7001/com.vehicle.rest/api/v2/inventory/<model>/<registration>
+	 * 
+	 * @param reg - vehicle registration number
+	 * @param model - vehicle model number
+	 * @return - json array results list from the database
+	 * @throws Exception
+	 */
+//	@Path("/{model}/{reg}")
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response returnSpecificModelRegs(
+//				@PathParam("model") String model,
+//				@PathParam("reg") String reg) 
+//				throws Exception {
+//		
+//		String returnString = null;
+//		
+//		JSONArray json = new JSONArray();
+//		
+//		try {
+//			
+//			SchemaVehicle dao = new SchemaVehicle();
+//			
+//			json = dao.queryReturnModelRegs(model, reg);
+//			returnString = json.toString();
+//		}
+//		catch (Exception e) {
+//			e.printStackTrace();
+//			return Response.status(500).entity("Server was not able to process your request").build();
+//		}
+//		
+//		return Response.ok(returnString).build();
+//	}
+	
+	/**
+	 * This method will allow you to insert data the VEHICLE_REGS table.  
+	 * This is a example of using the Jackson Processor
+	 * 
+	 * Note: If you look, this method addPcParts using the same URL as a GET method returnVehicleRegs.
+	 * 			We can do this because we are using different HTTP methods for the same URL string.
+	 * 
+	 * @param incomingData - must be in JSON format
+	 * @return String
+	 * @throws Exception
+	 */
+	@POST
+	@Consumes({MediaType.APPLICATION_FORM_URLENCODED,MediaType.APPLICATION_JSON}) // for both web form and application.JSON 
+	//@Consumes(MediaType.APPLICATION_FORM_URLENCODED) 							  // for only web forms
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addVehicleRegsPcParts(String incomingData) throws Exception {
+		
+		String returnString = null;
+		//JSONArray jsonArray = new JSONArray(); //not needed
+		SchemaVehicle dao = new SchemaVehicle();
+		
+		try {
+			System.out.println("incomingData: " + incomingData);
+			
+			/*
+			 * ObjectMapper is from Jackson Processor framework
+			 * http://jackson.codehaus.org/
+			 * 
+			 * Using the readValue method, you can parse the json from the http request
+			 * and data bind it to a Java Class.
+			 */
+			ObjectMapper mapper = new ObjectMapper();  
+			ItemEntry itemEntry = mapper.readValue(incomingData, ItemEntry.class);
+			
+			int http_code = dao.insertIntoVEHICLE_REGS(itemEntry.VEHICLE_REGS_REG, 
+													itemEntry.VEHICLE_REGS_BRAND, 
+													itemEntry.VEHICLE_REGS_MODEL, 
+													itemEntry.VEHICLE_REGS_MANU_YEAR, 
+													itemEntry.VEHICLE_REGS_OWNER_FIRST_NAME,
+													itemEntry.VEHICLE_REGS_OWNER_LAST_NAME,
+													itemEntry.VEHICLE_REGS_FIRST_REG );
+			
+			if( http_code == 200 ) {
+				//returnString = jsonArray.toString();
+				returnString = "Item inserted";
+			} else {
+				return Response.status(500).entity("Unable to process Item").build();
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(500).entity("Server was not able to process your request").build();
+		}
+		
+		return Response.ok(returnString).build();
+	}
+	
 }
+
+
+/*
+ * This is a class used by the addPcParts method.
+ * Used by the Jackson Processor
+ * 
+ * Note: for re-usability you should place this in its own package.
+ */
+
+class ItemEntry {
+	public String VEHICLE_REGS_REG;
+	public String VEHICLE_REGS_BRAND;
+	public String VEHICLE_REGS_MODEL;
+	public String VEHICLE_REGS_MANU_YEAR;
+	public String VEHICLE_REGS_OWNER_FIRST_NAME;
+	public String VEHICLE_REGS_OWNER_LAST_NAME;
+	public String VEHICLE_REGS_FIRST_REG;
+
+}
+
+
